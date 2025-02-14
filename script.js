d372f29a967081cfb701b798d8bd0a3ef0d2fa35
Diogo Gonzales Pascual <gonzalespascualdiogo7@gmail.com>
2025-02-13 23:12:47 -0500

    Actualización

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Estas segura?",
+    "Realmente segura:(??",
+    "Ni con una hamburguesa?",
+    "Porfis",
+    "Piensalo un ratito",
+    "Si dices que no, me mato...",
+    "Porfavorcito",
+    "Estaré muy triste",
+    "Esta bien, ya nada",
+    "Mentira, di si porfis❤️"
 ];
 
 let messageIndex = 0;
